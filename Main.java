a082e532bec6e4d85c89b45f71794ffbcd67dc9c
YJBeetle <YJBeetle@gmail.com>
2017-02-01 02:06:44 +0800

    正确读取了更多数据

--- Main.java
+++ Main.java
@@ -11,22 +11,51 @@ public class Main
 {
 	public static void main(String [] args) throws Exception
 	{
-		float e[];
+        float af[];
+        short aword0[];
+        short aword1[];
+        boolean flag;
 
+		String _fld02CA;
+		String _fld02CB;
+		String _fld02CE[];
 
 		try
 		{
 			FileInputStream fileIn = new FileInputStream("portalKeyResourceUnit.obj");
 			ObjectInputStream in = new ObjectInputStream(fileIn);
-			e = (float[])in.readObject();
+            af = (float[])in.readObject();
+            aword0 = (short[])in.readObject();
+            aword1 = (short[])in.readObject();
+
+			System.out.println(in.readInt());
+
+			in.readInt();
+			in.readInt();
+			in.readUTF();
+
+			flag = in.readBoolean();
+
+			_fld02CA = in.readUTF();
+			//_fld02CB = in.readUTF();
+			//_fld02CE = (String[])in.readObject();
+
 			in.close();
 			fileIn.close();
 
-            for(int i=0;i<e.length;i++){
-		        System.out.println(e[i]);
-            }
+            // for(int i=0;i<af.length;i++){
+		    //     System.out.println(af[i]);
+            // }
+            // for(int i=0;i<aword0.length;i++){
+		    //     System.out.println(aword0[i]);
+            // }
+            // for(int i=0;i<aword1.length;i++){
+		    //     System.out.println(aword1[i]);
+            // }
 		
-		    System.out.println(e.length);
+		    // System.out.println(af.length);
+		    // System.out.println(aword0.length);
+		    // System.out.println(aword1.length);
 
 		}catch(IOException i)
 		{
