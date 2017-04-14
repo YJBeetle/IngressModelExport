6d8ef95ef5d19c211f8ac985c15136aa0ec3a3df
YJBeetle <YJBeetle@gmail.com>
2017-04-14 16:01:01 +0800

    区分obj和dae的输出

--- Main.java
+++ Main.java
@@ -132,117 +132,130 @@ public class Main
         objectinputstream.close();
 		fileIn.close();
 
-		//解析准备
-		int vlen = 3;	//默认格式
-		int vtlen = 2;
-		int vnlen = 0;
-		String tmpString;
+		//拼接
 		String fileOutText = "";
-
-		//输出注释
-		fileOutText += "# Create by IngressModelExport" + "\n";
-		fileOutText += "# Develop by YJBeetle" + "\n";
-		fileOutText += "# Now time is " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "\n";
-		fileOutText += "" + "\n";
-
-		//原始信息输出
-		fileOutText += "# ingress obj info:" + "\n";
-		fileOutText += "# af.length = " + af.length + "\n";	//点数据量，格式：顶点坐标x + 顶点坐标y + 顶点坐标z + 顶点法线x + 顶点法线y + 顶点法线z + 贴图坐标x + 贴图坐标y
-		fileOutText += "# aword0.length = " + aword0.length + "\n";	//表面数据量，格式：顶点序号a + 顶点序号b + 顶点序号c
-		fileOutText += "# aword1.length = " + aword1.length + "\n";	//线数据量，格式：顶点序号a + 顶点序号b
-		fileOutText += "# avertexattribute.length = " + avertexattribute.length + "\n";
-		for(i = 0; i < avertexattribute.length; i++)
+		if(fileType == FileType.obj)	//拼接为obj
 		{
-			fileOutText += "# avertexattribute[" + i + "].usage = " + avertexattribute[i].usage + "\n";
-			fileOutText += "# avertexattribute[" + i + "].numComponents = " + avertexattribute[i].numComponents + "\n";
-			fileOutText += "# avertexattribute[" + i + "].alias = " + avertexattribute[i].alias + "\n";
-			if(avertexattribute[i].alias.equals("a_position"))	//v
-			{
-				vlen = avertexattribute[i].numComponents;
-			}
-			if(avertexattribute[i].alias.equals("a_texCoord0"))	//vt
-			{
-				vtlen = avertexattribute[i].numComponents;
-			}
-			if(avertexattribute[i].alias.equals("a_normal"))	//vn
-			{
-				vnlen = avertexattribute[i].numComponents;
-			}
-		}
-		fileOutText += "" + "\n";
+			//准备
+			int vlen = 3;	//默认格式
+			int vtlen = 2;
+			int vnlen = 0;
+			String tmpString;
 
-		//模型信息输出
-		fileOutText += "# obj info:" + "\n";
-		fileOutText += "# Vertex count: "+ af.length / (vlen+vtlen+vnlen) + "\n";
-		fileOutText += "# Surface count: "+ aword0.length / 3 + "\n";
-		fileOutText += "# Line count: "+ aword1.length / 2 + "\n";
-		fileOutText += "" + "\n";
+			//输出注释
+			fileOutText += "# Create by IngressModelExport" + "\n";
+			fileOutText += "# Develop by YJBeetle" + "\n";
+			fileOutText += "# Now time is " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "\n";
+			fileOutText += "" + "\n";
 
-		//顶点(v)
-		fileOutText += "# Geometric vertices (v):" + "\n";
-		for(i = 0; i < (af.length / (vlen+vtlen+vnlen)); i++)
-		{
-			tmpString = "";
-			for(ii = 0; ii < vlen; ii++)
-				tmpString += af[i * (vlen+vtlen+vnlen) + ii] + " ";
-			fileOutText += "v " + tmpString + "\n";
-		}
-		fileOutText += "" + "\n";
+			//原始信息输出
+			fileOutText += "# ingress obj info:" + "\n";
+			fileOutText += "# af.length = " + af.length + "\n";	//点数据量，格式：顶点坐标x + 顶点坐标y + 顶点坐标z + 顶点法线x + 顶点法线y + 顶点法线z + 贴图坐标x + 贴图坐标y
+			fileOutText += "# aword0.length = " + aword0.length + "\n";	//表面数据量，格式：顶点序号a + 顶点序号b + 顶点序号c
+			fileOutText += "# aword1.length = " + aword1.length + "\n";	//线数据量，格式：顶点序号a + 顶点序号b
+			fileOutText += "# avertexattribute.length = " + avertexattribute.length + "\n";
+			for(i = 0; i < avertexattribute.length; i++)
+			{
+				fileOutText += "# avertexattribute[" + i + "].usage = " + avertexattribute[i].usage + "\n";
+				fileOutText += "# avertexattribute[" + i + "].numComponents = " + avertexattribute[i].numComponents + "\n";
+				fileOutText += "# avertexattribute[" + i + "].alias = " + avertexattribute[i].alias + "\n";
+				if(avertexattribute[i].alias.equals("a_position"))	//v
+				{
+					vlen = avertexattribute[i].numComponents;
+				}
+				if(avertexattribute[i].alias.equals("a_texCoord0"))	//vt
+				{
+					vtlen = avertexattribute[i].numComponents;
+				}
+				if(avertexattribute[i].alias.equals("a_normal"))	//vn
+				{
+					vnlen = avertexattribute[i].numComponents;
+				}
+			}
+			fileOutText += "" + "\n";
 
-		//贴图坐标(vt)
-		if(vtlen > 0)
-		{
-			fileOutText += "# Texture vertices (vt):" + "\n";
+			//模型信息输出
+			fileOutText += "# obj info:" + "\n";
+			fileOutText += "# Vertex count: "+ af.length / (vlen+vtlen+vnlen) + "\n";
+			fileOutText += "# Surface count: "+ aword0.length / 3 + "\n";
+			fileOutText += "# Line count: "+ aword1.length / 2 + "\n";
+			fileOutText += "" + "\n";
+
+			//顶点(v)
+			fileOutText += "# Geometric vertices (v):" + "\n";
 			for(i = 0; i < (af.length / (vlen+vtlen+vnlen)); i++)
 			{
 				tmpString = "";
-				for(ii = 0; ii < vtlen; ii++)
-					tmpString += af[i * (vlen+vtlen+vnlen) + vlen + vnlen + ii] + " ";
-				fileOutText += "vt " + tmpString + "\n";
+				for(ii = 0; ii < vlen; ii++)
+					tmpString += af[i * (vlen+vtlen+vnlen) + ii] + " ";
+				fileOutText += "v " + tmpString + "\n";
+			}
+			fileOutText += "" + "\n";
+
+			//贴图坐标(vt)
+			if(vtlen > 0)
+			{
+				fileOutText += "# Texture vertices (vt):" + "\n";
+				for(i = 0; i < (af.length / (vlen+vtlen+vnlen)); i++)
+				{
+					tmpString = "";
+					for(ii = 0; ii < vtlen; ii++)
+						tmpString += af[i * (vlen+vtlen+vnlen) + vlen + vnlen + ii] + " ";
+					fileOutText += "vt " + tmpString + "\n";
+				}
+				fileOutText += "" + "\n";
+			}
+
+			//顶点法线(vn)
+			if(vnlen > 0)
+			{
+				fileOutText += "# Vertex normals (vn):" + "\n";
+				for(i = 0; i < (af.length / (vlen+vtlen+vnlen)); i++)
+				{
+					tmpString = "";
+					for(ii = 0; ii < vnlen; ii++)
+						tmpString += af[i * (vlen+vtlen+vnlen) + vlen + ii] + " ";
+					fileOutText += "vn " + tmpString + "\n";
+				}
+				fileOutText += "" + "\n";
+			}
+
+			//面(f)
+			fileOutText += "# Surface (f):" + "\n";
+			for(i = 0; i < (aword0.length/3); i++)
+			{
+				if(vtlen > 0 && vnlen > 0)
+				{
+					fileOutText += "f " + (aword0[i*3]+1) + "/" + (aword0[i*3]+1) + "/" + (aword0[i*3]+1) + " " + (aword0[i*3+1]+1) + "/" + (aword0[i*3+1]+1) + "/" + (aword0[i*3+1]+1) + " " + (aword0[i*3+2]+1) + "/" + (aword0[i*3+2]+1) + "/" + (aword0[i*3+2]+1) + "\n";
+				}
+				else if(vtlen > 0 && vnlen == 0)
+				{
+					fileOutText += "f " + (aword0[i*3]+1) + "/" + (aword0[i*3]+1) + " " + (aword0[i*3+1]+1) + "/" + (aword0[i*3+1]+1) + " " + (aword0[i*3+2]+1) + "/" + (aword0[i*3+2]+1) + "\n";
+				}
+				else
+				{
+					fileOutText += "f " + (aword0[i*3]+1) + " " + (aword0[i*3+1]+1) + " " + (aword0[i*3+2]+1) + "\n";
+				}
+			}
+			fileOutText += "" + "\n";
+
+			//线(l)
+			fileOutText += "# Line (l):" + "\n";
+			for(i = 0; i < (aword1.length/2); i++)
+			{
+				fileOutText += "l " + (aword1[i*2]+1) + " " + (aword1[i*2+1]+1) + "\n";
 			}
 			fileOutText += "" + "\n";
 		}
-
-		//顶点法线(vn)
-		if(vnlen > 0)
+		else if(fileType == FileType.dae)	//拼接dae
 		{
-			fileOutText += "# Vertex normals (vn):" + "\n";
-			for(i = 0; i < (af.length / (vlen+vtlen+vnlen)); i++)
-			{
-				tmpString = "";
-				for(ii = 0; ii < vnlen; ii++)
-					tmpString += af[i * (vlen+vtlen+vnlen) + vlen + ii] + " ";
-				fileOutText += "vn " + tmpString + "\n";
-			}
-			fileOutText += "" + "\n";
-		}
+			//文件头
+			fileOutText += "<?xml version=\"1.0\"?>\n";
+			fileOutText += "<COLLADA xmlns=\"http://www.collada.org/2005/11/COLLADASchema\" version=\"1.4.1\">\n";
 
-		//面(f)
-		fileOutText += "# Surface (f):" + "\n";
-		for(i = 0; i < (aword0.length/3); i++)
-		{
-			if(vtlen > 0 && vnlen > 0)
-			{
-				fileOutText += "f " + (aword0[i*3]+1) + "/" + (aword0[i*3]+1) + "/" + (aword0[i*3]+1) + " " + (aword0[i*3+1]+1) + "/" + (aword0[i*3+1]+1) + "/" + (aword0[i*3+1]+1) + " " + (aword0[i*3+2]+1) + "/" + (aword0[i*3+2]+1) + "/" + (aword0[i*3+2]+1) + "\n";
-			}
-			else if(vtlen > 0 && vnlen == 0)
-			{
-				fileOutText += "f " + (aword0[i*3]+1) + "/" + (aword0[i*3]+1) + " " + (aword0[i*3+1]+1) + "/" + (aword0[i*3+1]+1) + " " + (aword0[i*3+2]+1) + "/" + (aword0[i*3+2]+1) + "\n";
-			}
-			else
-			{
-				fileOutText += "f " + (aword0[i*3]+1) + " " + (aword0[i*3+1]+1) + " " + (aword0[i*3+2]+1) + "\n";
-			}
+			//结尾
+			fileOutText += "</COLLADA>\n";
 		}
-		fileOutText += "" + "\n";
-
-		//线(l)
-		fileOutText += "# Line (l):" + "\n";
-		for(i = 0; i < (aword1.length/2); i++)
-		{
-			fileOutText += "l " + (aword1[i*2]+1) + " " + (aword1[i*2+1]+1) + "\n";
-		}
-		fileOutText += "" + "\n";
 
 		//输出
 		if(fileOutPath == null)
